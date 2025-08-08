5c3b03ba4a4da19339a7101c029d0a3ca06f2e04
Piyush Raj Sharma <rpiyush1357@gmail.com>
2025-08-08 15:30:20 +0530

    Created the GET endpoint for fetching the post and added a variable my_posts as a dummy database

--- main.py
+++ main.py
@@ -4,7 +4,22 @@ from typing import Optional
 
 app = FastAPI()
 
-class post(BaseModel):
+my_posts = [
+    {
+        "title": 1,
+        "content" : "Learning FastAPI",
+        "published" : True,
+        "ratings" : "5 STAR"
+    },
+    {
+        "title": 2,
+        "content" : "Learned React/Redux",
+        "published" : False,
+        "ratings" : "4 STAR"
+    }
+]
+
+class Post(BaseModel):
     title: int
     content: str
     published: bool = True
@@ -14,3 +29,6 @@ class post(BaseModel):
 def root():
     return {"message" : "Hello from the social media app"}
 
+@app.get("/post")
+def get_posts():
+    return my_posts
