842f38849aca6e917451b74d131f299a96fa0b93
Piyush Raj Sharma <rpiyush1357@gmail.com>
2025-08-21 23:35:11 +0530

    Created GET endpoint to fetch all users

--- SQLALCHMY/main.py
+++ SQLALCHMY/main.py
@@ -67,7 +67,7 @@ def delete_post(post_id : int, db: Session = Depends(get_db) ):
 
 # -----------------------------------------------------------------------------
 
-app.post('/users', response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
+@app.post('/users', response_model=schemas.UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     new_user = models.User(**user.dict())
     db.add(new_user)
@@ -75,4 +75,8 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-app.get()
+@app.get('/users', response_model=list[schemas.UserResponse])
+def fetch_user(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
