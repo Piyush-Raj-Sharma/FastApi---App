11e6584f3a247cc17e03e2becefbda0243fc0894
Piyush Raj Sharma <rpiyush1357@gmail.com>
2025-08-26 01:49:32 +0530

    Added a function to verify credentials

--- SQLALCHMY/routers/login.py
+++ SQLALCHMY/routers/login.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from SQLALCHMY.orm import Session
 from ..database import get_db
-from .. import schemas
+from .. import schemas, models
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
 def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.user.email == user_credentials.email).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, details=f"Invalid Credentials")
