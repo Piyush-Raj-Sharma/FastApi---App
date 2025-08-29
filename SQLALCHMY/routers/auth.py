694254005b669f6c7519853aa271af341140ea57
Piyush Raj Sharma <rpiyush1357@gmail.com>
2025-08-30 00:30:01 +0530

    feat(auth): Add access token generation and switch login to OAuth2PasswordRequestForm

--- SQLALCHMY/routers/auth.py
+++ SQLALCHMY/routers/auth.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from ..database import get_db
 from .. import schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     
     if not user or not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
