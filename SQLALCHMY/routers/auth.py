5e09f5851f234e60baf9641cc960698660b3454c
Piyush Raj Sharma <rpiyush1357@gmail.com>
2025-08-29 23:59:45 +0530

    feat(auth): generate JWT access token with user_id claim during login

--- SQLALCHMY/routers/auth.py
+++ SQLALCHMY/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import schemas, models, utils
+from .. import schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
@@ -15,4 +15,7 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
             detail="Invalid Credentials"
         )
     
-    return {"message": "Login successful!"}
+    access_token = oauth2.create_access_token(data = {"user_id" : user.user_id}) 
+
+    return {"access_token": access_token, "token_type": "bearer"}
+ 
